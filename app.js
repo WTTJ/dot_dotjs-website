d8dcbb15c57a60067479641d3f0031ebfaf1d8d1
Thomas Bassetto <tbassetto@gmail.com>
2012-05-22 18:21:15 +0200

    Added logs for development and fixes small things

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.configure(function(){
 	app.use(express.static(__dirname + '/public'));
 });
 
+app.configure('development', function() {
+	console.log('deeeev');
+  app.use(express.logger());
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
 //Heroku
 var port = process.env.PORT || 3333;
 app.listen(port, function() {
