4235d3af34ae80d02b7a833ae544a2098f8b122c
Thomas Bassetto <tbassetto@gmail.com>
2012-05-22 19:02:50 +0200

    Switched to express 3.0.0alpha4

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ app.configure(function(){
 	app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function() {
-	console.log('deeeev');
-  app.use(express.logger());
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+// app.configure('development', function() {
+//   app.use(express.logger());
+//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+// });
 
 //Heroku
 var port = process.env.PORT || 3333;
