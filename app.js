48d3527914379e01a77b251778e47760cb51f345
Thomas Bassetto <tbassetto@gmail.com>
2012-05-22 17:30:50 +0200

    Better Facebook integration

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var express = require('express'),
-	app = express.createServer()
-
-// Reference
-// http://expressjs.com/guide.html
-// https://github.com/spadin/simple-express-static-server
-// http://devcenter.heroku.com/articles/node-js
+		app = express.createServer();
 
 // Configuration
 app.configure(function(){
@@ -12,29 +7,15 @@ app.configure(function(){
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 
-	// LESS Support
-	//app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
-	// Template-enabled html view (by jade)
-	// http://stackoverflow.com/questions/4529586/render-basic-html-view-in-node-js-express
-	//app.set('views', __dirname + '/app/views');
-	//app.register('.html', require('jade'));
-
 	//Error Handling
 	app.use(express.logger());
 	app.use(express.errorHandler({
-		dumpExceptions: true, 
+		dumpExceptions: true,
 		showStack: true
 	}));
 
 	//Setup the Route, you are almost done
 	app.use(app.router);
-});
-
-app.get('/', function(req, res){
-	//Apache-like static index.html (public/index.html)
-	res.redirect("/index.html");
-	//Or render from view
-	//res.render("index.html")
 });
 
 //Heroku
